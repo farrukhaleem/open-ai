e228ef54f44e18238a588215cec866fdb5e2b353
Farukh Aleem <farukh.emeriosoft@gmail.com>
2025-04-12 22:03:14 +0500

    config updated

--- app/page.js
+++ app/page.js
@@ -1,20 +1,26 @@
 
 import Chat from "./components/Chat";
 import ImageBox from "./components/ImageBox";
-
+import Head from 'next/head';
 
 export default function Home() {
 
   return (
-    <div className="flex justify-center p-10">
-      <main>
-        {/* <ImageBox />         */}
-        <div className="p-5 text-center">
-          <h1 className="text-6xl font-bold text-blue-200">Ask Krishna</h1>
-          <p className="text-xl font-semibold text-blue-200">Ask Your Problems, Krishna will Answer 🦚</p>
-        </div>
-        <Chat />
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>Ask Krishna</title>
+        <meta name="description" content="Ask Your Problems, Krishna will Answer" />
+      </Head>
+      <div className="flex justify-center p-10">
+        <main>
+          <ImageBox />
+          <div className="p-5 text-center">
+            <h1 className="text-6xl font-bold text-blue-200">Ask Krishna</h1>
+            <p className="text-xl font-semibold text-blue-200">Ask Your Problems, Krishna will Answer 🦚</p>
+          </div>
+          <Chat />
+        </main>
+      </div>
+    </>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // required for static site
   images: {
     unoptimized: true, // important for static export if you're using <Image>
 },
